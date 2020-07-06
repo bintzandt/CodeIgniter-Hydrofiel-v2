8da8c89bb6f7313b6042511dd4be320fe0ae8f54
Bram in 't Zandt <bintzandt@users.noreply.github.com>
2020-07-06 14:54:42 +0200

    Add a generic sendEmail function to the email_helper
    This function takes care of adding attachments and sets the mail type correctly

--- app/Helpers/email_helper.php
+++ app/Helpers/email_helper.php
@@ -1,44 +1,79 @@
 <?php
+
 use App\Entities\User;
 use Config\Services;
 
-if (! function_exists('sendResetEmail')){
+if (!function_exists('sendResetEmail')) {
 	/**
 	 * Send a reset password email.
 	 * 
 	 * @return bool Indicates whether the email was send succesfully.
 	 */
-	function sendResetPasswordEmail( User $user ): bool {
-		$email = Services::email();
+	function sendResetPasswordEmail(User $user): bool {
+		$from = [
+			'name' => lang('Email.passwordResetSender'),
+			'email' => 'no-reply@hydrofiel.nl',
+		];
 
-		return $email->setFrom('no-reply@hydrofiel.nl', lang('Email.passwordResetSender'))
-			->setTo($user->email)
-			->setSubject(lang('Email.passwordResetSubject'))
-			->setMessage(view(lang('Email.passwordResetPath'), [
+		return sendEmail(
+			$from,
+			$user->email,
+			[],
+			lang('Email.passwordResetSubject'),
+			view(lang('Email.passwordResetPath'), [
 				'recovery' => $user->recovery,
 				'valid' => $user->recovery_valid,
-			]))
-			->setMailType('html')
-			->send();
+			]),
+		);
 	}
 }
 
-if (! function_exists('sendUserUpdateEmail')){
+if (!function_exists('sendUserUpdateEmail')) {
 	/**
 	 * Send a user update email.
 	 * 
 	 * @return bool Indicates whether the email was send succesfully.
 	 */
-	function sendUserUpdateEmail( User $user ): bool {
-		$email = Services::email();
-
-		return $email->setFrom('no-reply@hydrofiel.nl', 'Ledennotificatie')
-			->setTo('secretaris@hydrofiel.nl')
-			->setSubject('Lid bewerkt')
-			->setMessage(view('email/updateNotification', [
+	function sendUserUpdateEmail(User $user): bool {
+		$from = [
+			'name' => 'Ledennotificatie',
+			'email' => 'no-reply@hydrofiel.nl',
+		];
+		return sendEmail(
+			$from,
+			'secretaris@hydrofiel.nl',
+			[],
+			'Lid bewerkt',
+			view('email/updateNotification', [
 				'naam'  => $user->name,
 				'email' => $user->email,
-			]))
+			]),
+		);
+	}
+}
+
+if (!function_exists('sendEmail')) {
+	/**
+	 * Sends an email.
+	 */
+	function sendEmail(array $from, string $to, array $bcc = [], string $subject, string $message, array $attachments = []): bool {
+		$email = Services::email();
+		$email->clear(true);
+
+		/**
+		 * Add attachments to email.
+		 * 
+		 * @var CodeIgniter\HTTP\Files\UploadedFile $attachment The attachment.
+		 */
+		foreach ($attachments as $attachment) {
+			$email->attach($attachment->getTempName(), "", $attachment->getName());
+		}
+
+		return $email->setFrom($from['email'], $from['name'])
+			->setTo($to)
+			->setBCC($bcc)
+			->setSubject($subject)
+			->setMessage($message)
 			->setMailType('html')
 			->send();
 	}
