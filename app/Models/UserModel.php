7bac5084396f20c8ebf5cca7a3b033211adaedf8
Bram in 't Zandt <bintzandt@users.noreply.github.com>
2020-07-06 14:56:10 +0200

    Add a function to the UserModel to fetch a group of users
    This is based on their lidmaatschap.

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -28,4 +28,34 @@ class UserModel extends Model {
 		);
 		return $query->getResult();
 	}
+
+	/**
+	 * Gets a list of recipients belonging to a certain group.
+	 */
+	public function getGroupRecipients( string $group, bool $english): array {
+		switch($group){
+			// This group consists of both recreative and competition players.
+			case 'waterpolo': $this->whereIn('lidmaatschap', ['waterpolo_competitie', 'waterpolo_recreatief']); break;
+			// This group consists of everyone that wants to receive the newsletter.
+			case 'nieuwsbrief': $this->where('nieuwsbrief', true);
+			// This group consists of the board, i.e. everyone with rank 2.
+			case 'bestuur': $this->where('rank', 2); break;
+			// This group consists of everyone, i.e. no constraints.
+			case 'iedereen': break;
+			// This is an empty group, meant for individual selections.
+			case 'select': return [];
+			/**
+			 * By default, filter on the provided group. This default applies to the following groups:
+			 * - trainers
+			 * - waterpolo_recreatief
+			 * - waterpolo_competitief
+			 * - zwemmers
+			 */
+			default: $this->where('lidmaatschap', $group); break;
+		}
+		return $this
+			->select('email')
+			->where('engels', $english)
+			->findAll();
+	}
 }
