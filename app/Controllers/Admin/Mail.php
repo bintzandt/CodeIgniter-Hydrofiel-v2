fa6a244a5e3eed63e58c0d61b46af2d48079ae60
Bram in 't Zandt <bintzandt@users.noreply.github.com>
2020-07-06 14:58:26 +0200

    Implement the Admin/Mail controller
    - Create a library that handles the data fetching
    - Add views for the mails

--- app/Controllers/Admin/Mail.php
+++ app/Controllers/Admin/Mail.php
@@ -4,18 +4,90 @@ namespace App\Controllers\Admin;
 
 use \App\Controllers\BaseController;
 use App\Models\UserModel;
+use App\Libraries\Mail as MailLibrary;
 
 class Mail extends BaseController {
+	protected MailLibrary $mailHelper;
+
 	public function __construct() {
-		helper(['form']);
+		helper(['form', 'email']);
+		$this->mailHelper = new MailLibrary();
 	}
 
+	/**
+	 * Displays a form where the board can send an email.
+	 */
 	public function index() {
 		$users = new UserModel();
 		return view('admin/mail/index', ['leden' => $users->findAll()]);
 	}
 
-	public function handleEmailFormSubmission(){
-		var_dump($this->request->getPost()); exit;
+	/**
+	 * Handles the submission of the form.
+	 * Sends an email to the chosen groups.
+	 */
+	public function handleEmailFormSubmission() {
+		// Get data and prepare generic email settings.
+		$data = $this->request->getPost();
+		$from = $this->mailHelper->getFrom($data['van']);
+
+		// Prepare Dutch email.
+		$dutchAttachments = $this->request->getFileMultiple('attachments_nl');
+		$dutchRecipients = $this->getArrayOfRecipients(false);
+		$dutchSubject = $data['onderwerp'];
+		$dutchMessage = $this->mailHelper->getMessageContent(
+			$data['layout'],
+			[
+				'content' => $data['content'],
+				'engels' => false,
+			]
+		);
+
+		// Prepare English email.
+		$englishAttachments = $this->request->getFileMultiple('attachments_en');
+		$englishRecipients = $this->getArrayOfRecipients(true);
+		$englishSubject = $data['en_onderwerp'];
+		$englishMessage = $this->mailHelper->getMessageContent(
+			$data['layout'],
+			[
+				'content' => $data['en_content'],
+				'engels' => true
+			]
+		);
+
+		if (
+			sendEmail($from, 'no-reply@hydrofiel.nl', $dutchRecipients, $dutchSubject, $dutchMessage, $dutchAttachments) &&
+			sendEmail($from, 'no-reply@hydrofiel.nl', $englishRecipients, $englishSubject, $englishMessage, $englishAttachments)
+		) {
+			return redirect()->back()->with('success', 'Mail is verstuurd');
+		}
+
+		return redirect()->back()->with('error', 'Er ging iets fout');
+	}
+
+	/**
+	 * Returns an array of recipients based on the data posted in the form.
+	 * 
+	 * @param bool $english
+	 * 
+	 * @return string[] An array of emails.
+	 */
+	private function getArrayOfRecipients(bool $english): array {
+		$data = $this->request->getPost();
+		$recipients = [];
+
+		// We assume that custom recipients are Dutch.
+		if (!$english) {
+			$this->mailHelper->getCustomRecipients($data['email'],  $recipients);
+		}
+
+		$this->mailHelper->getGroupRecipients($data['aan'], $english, $recipients);
+
+		// Check if recipients were selected separately.
+		if (isset($data['los'])) {
+			$this->mailHelper->getSelectedRecipients($data['los'], $english, $recipients);
+		}
+
+		return $recipients;
 	}
 }
---
+++ app/Views/email/newsletter.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div style="width: 100%; margin:0 auto;" align="center">
+    <table cellspacing="0" cellpadding="0" width="100%">
+        <tr style="background: #ffab3a; height: 120px; color: #FFFFFF; font-size:25px; padding: 7px; ">
+            <td align="center"><img src="<?= site_url('images/logomail.png') ?>" alt="Logo" height="100px"
+                                    style="float:left; align-content: center"></td>
+            <td style="padding-right: 10px"><br/>
+                <h2><?= $engels ? "Hydrofiel Newsletter" : "Hydrofiel Nieuwsbrief" ?></h2></td>
+        </tr>
+        <tr>
+            <td rowspan="2" valign="top" style="text-align:left;"><?= $content ?></td>
+            <td style="background:#213947; font-size: 20px; padding: 5px; width: 150px; height:40px;"><a
+                        href="<?= site_url('/event/') ?>" style="color:#FFAB3A; text-decoration: none">
+                    <h3><?= $engels ? "Calendar" : "Agenda" ?></h3></a></td>
+        </tr>
+        <tr>
+            <td style="color:#FFFFFF; background:#315265; padding: 5px;" valign="top">
+                <?php if (!empty($events)) {
+                    foreach ($events as $event) { ?>
+                        <div>
+							<span><a href="<?= site_url('/event/' . $event->event_id) ?>" style="color: #FFF;"><img
+                                            style="padding-right: 5px" width="16px" height="16px"
+                                            src="<?= site_url(
+                                                '/images/mail/calendar.png'
+                                            ) ?>"><?= $engels ? $event->en_naam : $event->nl_naam ?></a></span><br/>
+                            <?= $punt->van->format('d-m-Y H:i') ?>
+                        </div><br/>
+
+                    <?php }
+                } ?></td>
+        </tr>
+        <tr>
+            <td style="color: #FFFFFF; background: #315265; padding: 7px;" height="50" colspan="2"></td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
